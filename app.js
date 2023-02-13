bc7517dc4cd2e6f5d5c32838778477cb9d2539bf
Sniphs98 <lukas@bronstering.com>
2023-02-13 20:10:57 +0100

    now mulitcore

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const serverIndex = require('serve-index')
+const copression = require('compression')
+const cluster = require('cluster')
+const os = require('os')
 const path = require('path')
 
 const app = express()
 
+const numberOfCpuCors = os.cpus().length
+
 app.set('view engine', 'ejs')
-app.use(fileUpload(),express.json())
+app.use(
+    fileUpload(),
+    express.json(),
+    copression())
 
 app.get('/', async (req, res, next) => {
     res.render('index')
@@ -27,4 +35,11 @@ app.post('/single', async(req, res, next) => {
 
 })
 
-app.listen(3000, () => console.log('🚀 on port: 3000 | http://localhost:3000/'))
+if (cluster.isMaster) {
+    for (let i = 0; i < numberOfCpuCors; i++){
+        cluster.fork()
+    }
+} else {
+    app.listen(3000, () => console.log( process.pid + ' 🚀 on port: 3000 | http://localhost:3000/'))
+}
+
