4d79bd1bcbc5f687929c4e6b4e285e47c3607fb3
Sniphs98 <lukas@bronstering.com>
2023-03-12 02:42:59 +0100

    update git ignore and final upload card

--- Frontend/src/routes/ItemUploadCard.svelte
+++ Frontend/src/routes/ItemUploadCard.svelte
@@ -6,10 +6,11 @@
   let loading = false;
 
   let file:any = {};
+  let fileName:string = "Upload file";
 
   async function upload() {
     const selectedFile = file
-    const fileName = selectedFile.name;
+    fileName = selectedFile.name;
     
     let header = new Headers();
     header.append('Content-Name', fileName)
@@ -31,7 +32,7 @@
     input.id = 'input';
     input.type = 'file';
     input.onchange = ()  => {
-        let files =   Array.from(input.files);
+        let files = Array.from(input.files);
         file = files[0];
         fileProxy.file = files[0];
       };
@@ -39,19 +40,24 @@
   }
 
   var fileProxy = new Proxy(file, {
-    set: function (target, key, value) {        
-        console.log(isEmpty(value));
-        
+    set: function (target, key, value) {              
         if(isEmpty(value)){
           document.getElementById("uploadButton").disabled = true;
+          document.getElementById("deleteButton").disabled = true;
+          fileName = "Upload file"
         }else{
+          fileName = value.name
           document.getElementById("uploadButton").disabled = false;
+          document.getElementById("deleteButton").disabled = false;
         }
-        
         target[key] = value;
         return true;
     }
   });
+
+  function removeFile(){
+    fileProxy.file = {};
+  }
 
   function isEmpty(obj:any) {
     if(Object.keys(obj).length === 0 && Object.getPrototypeOf(obj) === Object.prototype){
@@ -61,18 +67,21 @@
   }
 
 </script>
-
-  <div class="container">
-    {#if loading}
-      <Loading></Loading>
-    {:else}
-      <div on:click={openFileDialog} class="card-container" id="card-container">
-        <img id="upload-image" src="src/icons/upload-image.png">
-        <label>Upload files</label>
-      </div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="container">
+  {#if loading}
+    <Loading></Loading>
+  {:else}
+    <div on:click={openFileDialog} class="card-container" id="card-container">
+      <img id="upload-image" src="src/icons/upload-image.png">
+      <label>{fileName}</label>
+    </div>
+    <div class="button-container">
       <button disabled on:click={upload} class="uploadButton" id="uploadButton">Upload</button>
-    {/if}
-  </div>
+      <button disabled class="delete" id="deleteButton" on:click={removeFile}><i class="fa fa-close"></i></button>
+    </div>
+  {/if}
+</div>
   
 <style lang="scss">
   img {
@@ -116,11 +125,18 @@
     box-shadow: 0 0 20px 8px rgb(61, 61, 61);
   }
 
+  .button-container{
+    display: flex;
+  }
+
   .uploadButton{
-    margin: 10px;
+    margin:        10px;
+    margin-right:   0px;
     background:    #3e6ef2;
     border-radius: 11px;
-    padding:       20px 45px;
+    border-top-right-radius: 0px;
+    border-bottom-right-radius: 0px;
+    padding:       20px 15px;
     color:         #ffffff;
     display:       inline-block;
     font:          normal bold 26px/1 "Open Sans", sans-serif;
@@ -142,6 +158,32 @@
     background:    #7f95db;
     box-shadow: 0 2px #666;
     transform: translateY(3px);
+  }
+
+  .delete{
+    margin:        10px;
+    margin-left:   0px;
+    background:    #f23e3e;
+    border-radius: 11px;
+    border-top-left-radius: 0px;
+    border-bottom-left-radius: 0px;
+    padding:       20px 15px;
+    color:         #ffffff;
+    display:       inline-block;
+    font:          normal bold 26px/1 "Open Sans", sans-serif;
+    text-align:    center;
+    border:        none;
+    box-shadow:    0 5px #666;
+  }
+
+  .delete:hover{
+    background:   #e62828;
+  }
+
+  .delete:disabled{
+  background:     #fc6d6d;
+    box-shadow:     0 2px #666;
+    transform:      translateY(3px);
   }
   
 </style>
